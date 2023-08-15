80ec159fc14b76d5249a715fb5f539204b8c6824
Eduard Karnaukh <ed@magesoft.co>
2023-08-15 14:59:38 -0400

    tenancy provider

--- src/HakamMultiTenancyBundle.php
+++ src/HakamMultiTenancyBundle.php
@@ -2,6 +2,9 @@
 
 namespace Hakam\MultiTenancyBundle;
 
+use Symfony\Bridge\Doctrine\DependencyInjection\Security\UserProvider\EntityFactory;
+use Symfony\Bundle\SecurityBundle\DependencyInjection\SecurityExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
@@ -9,4 +12,16 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class HakamMultiTenancyBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        if ($container->hasExtension('security')) {
+            $security = $container->getExtension('security');
+
+            if ($security instanceof SecurityExtension) {
+                $security->addUserProviderFactory(new EntityFactory('tenancy', 'hakam.multi_tenancy.user.provider'));
+            }
+        }
+    }
 }
